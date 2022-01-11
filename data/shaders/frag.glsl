b12b2abce4c039ef7e7b90233c2e3f81a58d31b2
yatsukha <yatsukha.roman@gmail.com>
2022-01-11 04:02:36 +0000

    Shader cleanup.

--- data/shaders/frag.glsl
+++ data/shaders/frag.glsl
@@ -13,29 +13,27 @@ uniform int max_steps;
 
 float mandelbulb_de(vec3 pos) {
   const float Bailout = 256.0;
-	vec3 z = pos;
-	float dr = 1.0;
-	float r = 0.0;
-	for (int i = 0; i < iterations ; i++) {
-		r = length(z);
-		if (r > Bailout) break;
-		
-		// convert to polar coordinates
-		float theta = acos(z.z/r);
-		float phi = atan(z.y,z.x);
-		dr = pow(r, power - 1.0) * power * dr + 1.0;
-		
-		// scale and rotate the point
-		float zr = pow(r, power);
-		theta = theta * power;
-		phi = phi * power;
-		
-		// convert back to cartesian coordinates
-		z = zr*vec3(sin(theta)*cos(phi), sin(phi)*sin(theta), cos(theta));
-    //z = zr*vec3( cos(theta)*cos(phi), cos(theta)*sin(phi), sin(theta));
-		z += pos;
-	}
-	return 0.5 * log(r) * r / dr;
+  vec3 z = pos;
+  float dr = 1.0;
+  float r = 0.0;
+  for (int i = 0; i < iterations ; i++) {
+    r = length(z);
+
+    if (r > Bailout) break;
+    
+    float theta = acos(z.z/r);
+    float phi = atan(z.y,z.x);
+    dr = pow(r, power - 1.0) * power * dr + 1.0;
+    
+    float zr = pow(r, power);
+    theta = theta * power;
+    phi = phi * power;
+    
+    z = zr * vec3(sin(theta) * cos(phi), sin(phi) * sin(theta), cos(theta));
+    //z = zr * vec3(cos(theta) * cos(phi), cos(theta) * sin(phi), sin(theta));
+    z += pos;
+  }
+  return 0.5 * log(r) * r / dr;
 }
 
 float sierpinski_de(vec3 z) {
@@ -44,13 +42,13 @@ float sierpinski_de(vec3 z) {
   float r;
   int n = 0;
   while (n < iterations) {
-     if(z.x+z.y<0) z.xy = -z.yx; // fold 1
-     if(z.x+z.z<0) z.xz = -z.zx; // fold 2
-     if(z.y+z.z<0) z.zy = -z.yz; // fold 3	
-     z = z*Scale - Offset*(Scale-1.0);
-     n++;
+    if (z.x + z.y < 0) z.xy = -z.yx; // fold 1
+    if (z.x + z.z < 0) z.xz = -z.zx; // fold 2
+    if (z.y + z.z < 0) z.zy = -z.yz; // fold 3	
+    z = z * Scale - Offset * (Scale - 1.0);
+    n++;
   }
-  return (length(z) ) * pow(Scale, -float(n));
+  return length(z) * pow(Scale, -float(n));
 }
 
 float distance_from_sphere(in vec3 p, in vec3 c, float r) {
@@ -65,7 +63,6 @@ float balls_de(in vec3 p) {
 
 struct march_result {
   vec3 position;
-  vec4 color;
   int steps;
   float distance;
 };
@@ -75,7 +72,6 @@ struct march_result {
 march_result ray_march(in vec3 ro, in vec3 rd) {
   float distance_traveled = 0.0;
 
-  //const float min_distance = 0.01;
   const float MAXIMUM_TRACE_DISTANCE = 100.0;
 
   for (int i = 0; i < max_steps; ++i) {
@@ -84,7 +80,7 @@ march_result ray_march(in vec3 ro, in vec3 rd) {
     vec4 color;
     float closest = mandelbulb_de(current_position);
     if (closest < min_distance) {
-      return march_result(current_position, color, i + 1, distance_traveled);
+      return march_result(current_position, i + 1, distance_traveled);
     }
 
     distance_traveled += closest;
@@ -95,7 +91,6 @@ march_result ray_march(in vec3 ro, in vec3 rd) {
 
   return march_result(
     ro + distance_traveled * rd,
-    vec4(0.0),
     max_steps, 
     -1.0
   );
@@ -120,7 +115,8 @@ void main() {
 
   vec3 cam_vec = camera_target - camera_position;
 
-  vec3 rd = normalize(cam_vec) + rotateAxis(up, normalize(cam_vec), angle) * length(uv);
+  vec3 rd = normalize(cam_vec) 
+    + rotateAxis(up, normalize(cam_vec), angle) * length(uv);
   rd *= 0.5;
   march_result mr = ray_march(camera_position, rd);
 
